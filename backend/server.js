1bba79f81a1fb5f8f9d1e7861c809fcc8d4980d9
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-25 17:55:53 +0545

    setting up

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
-const express= require('express');
+// const express= require('express');
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+}); 
