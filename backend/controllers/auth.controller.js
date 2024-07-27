ecac8ff3ffef532eebfb1850efb693fcd1cfa86a
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-27 23:28:34 +0545

    move TMDB Api setting

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -124,3 +124,17 @@ export async function logout(req, res) {
     }
 }
 
+
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
