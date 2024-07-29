9bf2cb969d359a6184df973c67c3cad7275b9d13
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-29 22:41:16 +0545

    sucess api for movie details

--- backend/controllers/movie.contoller.js
+++ backend/controllers/movie.contoller.js
@@ -27,6 +27,15 @@ export async function getMovieTrailers(req, res) {
 }
 
 export async function getMovieDetails(req, res) {
+const {id}=req.params;
+  try{
+    const data = await fetcchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+    res.json({success:true,content:data});
 
-  
+  }
+  catch (error) {
+    if(error.message.includes("404")){
+      res.status(404).json({success:false, message: "Movie not found"});
+  }
+  }
 }
