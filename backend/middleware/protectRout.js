22d64956e42006ff5c6e8378d864bdd072ddb287
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-10 23:12:21 +0545

    drecpting the password

--- backend/middleware/protectRout.js
+++ backend/middleware/protectRout.js
@@ -7,10 +7,14 @@ try{
     const token=req.cookies['jtw-netflix']
 
     if(!token){
-        return res.status(401).json({message: 'You are not authorized to access this route'});
+        return res.status(401).json({success:false,message: 'You are not authorized to access this route'});
     }
 
     const decoded = jwt.verify(token, ENC_VARS.JWT_SECRET);
+
+    if(!decoded){
+        return res.status(401).json({success:false,message: 'You are not authorized to access this route'});
+    }
 
 }
 catch(error){
