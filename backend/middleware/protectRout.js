eaeff4a9cb3d0e65850d0ee94b5a42526d190c80
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-10 23:07:09 +0545

    importing coki parser

--- backend/middleware/protectRout.js
+++ backend/middleware/protectRout.js
@@ -5,6 +5,11 @@ import { ENC_VARS } from '../config/envVars.js';
 export const protectRoute = async (req, res, next) => {
 try{
     const token=req.cookies['jtw-netflix']
+
+    if(!token){
+        return res.status(401).json({message: 'You are not authorized to access this route'});
+    }
+    
 }
 catch(error){
     console.log("error from protectRoute",error.message);
