1a9a95b48771155cf55f69c3da46135ac02ea0ca
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-11 23:04:36 +0545

    aip for the search controller

--- backend/controllers/search.contoller.js
+++ backend/controllers/search.contoller.js
@@ -1,8 +1,3 @@
-import express from 'express';
-import { searchPerson, searchMovie, searchTv } from '../controllers/search.contoller.js';
-
-const route = express.Router();
-
 export async function searchPerson(req, res) {
 
 }
@@ -11,5 +6,3 @@ export async function searchMovie(req, res) {
 }export async function searchTv(req, res) {
 
 }
-
-export default route;
--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { searchPerson, searchMovie, searchTv } from '../controllers/search.controller.js';
+
 const route = express.Router();
 
 route.get("/person/:query", searchPerson);
-route.get("/move/:query", searchPerson);
-route.get("/tv/:query", searchPerson);
+route.get("/move/:query", searchMovie);
+route.get("/tv/:query", searchTv);
 
 export default route;
