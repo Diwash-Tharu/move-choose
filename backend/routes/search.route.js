3ca7697d4b448b702301726541a68c113641f2f7
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-11 22:49:47 +0545

    contoller for the serach for  user in the code

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -2,4 +2,8 @@ import express from 'express';
 
 const route = express.Router();
 
+route.get("/person/:query", searchPerson);
+route.get("/move/:query", searchPerson);
+route.get("/tv/:query", searchPerson);
+
 export default route;
