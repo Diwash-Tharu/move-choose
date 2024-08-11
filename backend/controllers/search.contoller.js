328b7f06209681602239d9889f18d29e9914f814
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-11 23:01:32 +0545

    setiing the api for search

--- backend/controllers/search.contoller.js
+++ backend/controllers/search.contoller.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { searchPerson, searchMovie, searchTv } from '../controllers/search.contoller.js';
 
+const route = express.Router();
+
 export async function searchPerson(req, res) {
 
 }
 export async function searchMovie(req, res) {
-    
+
 }export async function searchTv(req, res) {
-    
+
 }
+
+export default route;
