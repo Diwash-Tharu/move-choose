ad8807c571b1ee43a7ab47e23ce11a5463adcea8
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-11 23:20:08 +0545

    aip for the search controller

--- backend/controllers/search.contoller.js
+++ backend/controllers/search.contoller.js
@@ -1,8 +1,19 @@
 export async function searchPerson(req, res) {
 
+    //fetch('https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1', options)
+    const {query} = req.params;
+    try
+    {
+        const response = await fetch(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`, options);
+    }
+    catch (error) {
+        res.status(500).json({success:false, message: "error from searchPerson"+ error.message });
+    }
 }
 export async function searchMovie(req, res) {
 
+
 }export async function searchTv(req, res) {
+
 
 }
