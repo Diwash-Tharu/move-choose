956cd4225bf4e9dd9964efa3721debb84cb02919
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-12 22:44:08 +0545

    aip search move history

--- backend/controllers/search.contoller.js
+++ backend/controllers/search.contoller.js
@@ -6,7 +6,8 @@ export async function searchPerson(req, res) {
     const {query} = req.params;
     try
     {
-        const response = await fetcchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+
+        const response = await fetcchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`);
         if(response.results.length === 0)
         {
             return res.status(404).send(null);
@@ -27,7 +28,7 @@ export async function searchPerson(req, res) {
                     title:response.results[0].name,
                     SearchType: 'person',
                     createdAt: new Date()
-                    
+
                 }
             }
         }
@@ -40,7 +41,13 @@ export async function searchPerson(req, res) {
     }
 }
 export async function searchMovie(req, res) {
+const {query} = req.params;
+try{
 
+    const response = await fetcchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`);
+}
+catch (error) {
+    res.status(500).json({success:false, message: "error from searchMovie"+ error.message });
 
 }export async function searchTv(req, res) {
 
