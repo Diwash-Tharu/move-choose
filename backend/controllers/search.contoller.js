c5c07103247961c655d82af016303b2d762b2efc
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-15 23:02:29 +0545

    aip for tv show

--- backend/controllers/search.contoller.js
+++ backend/controllers/search.contoller.js
@@ -8,6 +8,7 @@ export async function searchPerson(req, res) {
     {
 
         const response = await fetcchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`);
+        
         if(response.results.length === 0)
         {
             return res.status(404).send(null);
@@ -44,7 +45,7 @@ export async function searchMovie(req, res) {
     const {query} = req.params;
     try{
 
-        const response = await fetcchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`);
+        const response = await fetcchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`);
     }
     catch (error) {
         res.status(500).json({success:false, message: "error from searchMovie"+ error.message });
