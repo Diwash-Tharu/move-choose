53d80612edf384782b36e4b477cabb13761a1be1
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-16 22:49:45 +0545

    api search history

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { searchPerson, searchMovie, searchTv } from '../controllers/search.controller.js';
+import { get } from 'mongoose';
 
 const route = express.Router();
 
@@ -8,6 +9,6 @@ route.get("/person/:query", searchPerson);
 route.get("/move/:query", searchMovie);
 route.get("/tv/:query", searchTv);
 
-route.get("/person/:query", searchPerson);
+route.get("/history", getSearchHistory);
 
 export default route;
