313762e6c3972e05135a807edddf18abbe26239f
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-08-16 23:01:25 +0545

    remove items  form search history

--- backend/controllers/search.contoller.js
+++ backend/controllers/search.contoller.js
@@ -134,3 +134,6 @@ export async function removeItemFromSearchHistory(req, res) {
         res.status(500).json({success:false, message: "error from removeItemFromSearchHistory"+ error.message });
     }
 }
+
+export async function clearSearchHistory(req, res) {
+}
